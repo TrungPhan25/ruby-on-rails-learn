7241586a49e173fc5f402cd3a0d57065025bd075
Phan Thế Trung (Toby) <toby@cybridge.jp>
2024-05-30 15:02:54 +0700

    Feature comment - rich text

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-  resources :posts
+  
+  resources :posts do
+    resources :comments
+  end
 
   get 'u/:id', to: 'users#profile', as: 'user'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
