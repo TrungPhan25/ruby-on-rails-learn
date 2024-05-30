6d4312bc91e111846162d678376e103bab321443
Phan Thế Trung (Toby) <toby@cybridge.jp>
2024-05-30 14:30:44 +0700

    part 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_29_093402) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_30_071624) do
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_unicode_520_ci", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -29,6 +29,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_29_093402) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.integer "views", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
